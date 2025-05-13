f1063acc3938d68689405c14893aa84d3b9f47e2
rakesh aragodi <rakesharagodi@gmail.com>
2025-05-13 22:18:04 +0530

    update

---
+++ node-web-app/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 80;  // Port 80 to make it public
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js running in Docker on EC2!');
+});
+
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
+});
---
+++ redis-image/app.py
@@ -0,0 +1,1 @@
+print("This is the from the application")
